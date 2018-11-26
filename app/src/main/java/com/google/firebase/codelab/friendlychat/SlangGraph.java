bb17c1101b3226fcf847a541d1e6db3f9f90bff3
yjo5252 <gracejo0227@gmail.com>
2018-11-26 21:08:17 +0900

    graph view

---
+++ app/src/main/java/com/google/firebase/codelab/friendlychat/SlangGraph.java
@@ -0,0 +1,13 @@
+package com.google.firebase.codelab.friendlychat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SlangGraph extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slang_graph);
+    }
+}
